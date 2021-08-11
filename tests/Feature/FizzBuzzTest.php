7fefca0e34a16030f0dc6c8ea16749e2b8986783
sait0928 <y.saitou928@gmail.com>
2021-08-11 09:59:12 +0900

    数字を1から順番に出力するテストを作成

--- tests/Feature/FizzBuzzTest.php
+++ tests/Feature/FizzBuzzTest.php
@@ -6,7 +6,12 @@ use Tests\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    // 数字を1から順番に出力
+    /** @test */
+    public function 数字を1から順番に出力(): void
+    {
+        $sut = new FizzBuzz();
+    }
+
     // 数字が3で割り切れる時は「Fizz」を出力
     // 数字が5で割り切れる時は「Buzz」を出力
     // 数字が3でも5でも割り切れる時は「FizzBuzz」を出力
