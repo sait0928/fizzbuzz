34364a7059c2e2c3f7e70f22b1cef571f64860d7
sait0928 <y.saitou928@gmail.com>
2021-08-11 10:24:12 +0900

    要件の記述を変更

--- tests/Unit/FizzBuzzTest.php
+++ tests/Unit/FizzBuzzTest.php
@@ -8,12 +8,12 @@ use Tests\TestCase;
 class FizzBuzzTest extends TestCase
 {
     /** @test */
-    public function 数字を1から順番に出力(): void
+    public function 指定された数値を文字列化して返す(): void
     {
         $sut = new FizzBuzz();
     }
 
-    // 数字が3で割り切れる時は「Fizz」を出力
-    // 数字が5で割り切れる時は「Buzz」を出力
-    // 数字が3でも5でも割り切れる時は「FizzBuzz」を出力
+    // 指定された数値が3で割り切れる時は「Fizz」を出力
+    // 指定された数値が5で割り切れる時は「Buzz」を出力
+    // 指定された数値が3でも5でも割り切れる時は「FizzBuzz」を出力
 }
