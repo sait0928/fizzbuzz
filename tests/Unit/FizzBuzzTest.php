dd1a448bd74d081b535aa2507afe06cc1dfe6640
sait0928 <y.saitou928@gmail.com>
2021-08-11 11:03:50 +0900

    新しくテストを作成

--- tests/Unit/FizzBuzzTest.php
+++ tests/Unit/FizzBuzzTest.php
@@ -13,12 +13,17 @@ class FizzBuzzTest extends TestCase
         $sut = new FizzBuzz();
         $this->assertSame('1', $sut->execute(1));
         $this->assertSame('2', $sut->execute(2));
-        $this->assertSame('3', $sut->execute(3));
         $this->assertSame('4', $sut->execute(4));
         $this->assertSame('5', $sut->execute(5));
     }
 
-    // 指定された数値が3で割り切れる時は「Fizz」を出力
+    /** @test */
+    public function 指定された数値が3で割り切れる時は「Fizz」を出力(): void
+    {
+        $sut = new FizzBuzz();
+        $this->assertSame('Fizz', $sut->execute(3));
+    }
+
     // 指定された数値が5で割り切れる時は「Buzz」を出力
     // 指定された数値が3でも5でも割り切れる時は「FizzBuzz」を出力
 }
