b0f9154d3a5f3aa7e0fb1cf0f41f3dd6cdf568fe
sait0928 <y.saitou928@gmail.com>
2021-08-11 11:21:01 +0900

    3でも5でも割り切れる場合はFizzBuzz

--- app/FizzBuzz/FizzBuzz.php
+++ app/FizzBuzz/FizzBuzz.php
@@ -8,6 +8,10 @@ class FizzBuzz
 {
     public function execute(int $i): string
     {
+        if ($i % 3 === 0 && $i % 5 === 0) {
+            return 'FizzBuzz';
+        }
+
         if ($i % 5 === 0) {
             return 'Buzz';
         }
